d0a93e12e534a8c95a07408d3c550874d26bd5d5
陈飞 <leo@chenfeideMac-mini.local>
2020-09-14 17:30:35 +0800

    添加全局样式处理

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'antd-mobile/dist/antd-mobile.css';  // or 'antd-mobile/dist/antd-mobile.less'
+import './index.css' // 全局样式
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Button } from 'antd-mobile';
 
 export default class CityList extends React.Component {
     render() {
         return (
-            <div>首页</div>
+            <div>首页
+                <Button type="primary">按钮</Button>
+                <Button type="warning">按钮</Button>
+                <Button type="ghost">按钮</Button>
+            </div>
         )
     }
 }
